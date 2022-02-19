80bcd353b6c02a53eee5cdeb000bd624ce05c18b
Daniel Cavazos <DCaazy@gmail.com>
2022-02-18 17:17:34 -0800

    Add Psytrainers and neuromod retextures (#30)
    * Add textures for neuromods
    
    * Create recipes for psytrainers, added to the fabrication bench

---
+++ Source/Utilities/Neuromods.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Verse;
+
+namespace Typhon
+{
+    internal class Neuromods : IEnumerable<ThingDef>
+	{
+		public IEnumerator<ThingDef> GetEnumerator()
+		{
+			foreach (ThingDef thingDef in DefDatabase<ThingDef>.AllDefs)
+			{
+				if (
+					thingDef.thingCategories.Contains(ThingCategoryDefOf.NeurotrainersPsycast)
+					|| thingDef.thingCategories.Contains(ThingCategoryDefOf.NeurotrainersSkill)
+					)
+					yield return thingDef;
+			}
+			yield break;
+		}
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+    }
+}
