45dd01bc651ba8eb8d89c498fd1ca11023152d63
Daniel Caaz <dcaazy@gmail.com>
2022-03-20 13:07:47 -0700

    Place walls on missed shots!

---
+++ Source/Projectile_Gloo.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Typhon
+{
+    internal class Projectile_Gloo : Bullet
+	{
+		protected override void Impact(Thing hitThing)
+		{
+			if (hitThing == null)
+			{
+				Building wall = (Building)ThingMaker.MakeThing(ThingDefOf.Wall, ThingDefOf.Steel);
+				bool wouldWipe = GenSpawn.WouldWipeAnythingWith(Position, Rotation, ThingDefOf.Wall, Map, delegate(Thing thing) {
+					return (thing.def.thingClass == typeof(Building));
+				});
+				if(!wouldWipe)
+					GenSpawn.Spawn(wall, Position, Map, Rotation);
+			}
+			base.Impact(hitThing);
+		}
+	}
+}
