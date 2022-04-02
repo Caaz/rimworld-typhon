6f615aa2f6cf5c777337097bc9530680b1c4253b
Daniel Caaz <dcaazy@gmail.com>
2022-04-01 17:29:30 -0700

    Make nests explode when near

---
+++ Source/CompProperties/ExplosiveNest.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace Typhon.CompProperties
+{
+    internal class ExplosiveNest : Verse.CompProperties
+    {
+        public PawnKindDef pawnKind = null;
+        public int amount = 0;
+        public float radius = 0;
+        public ExplosiveNest()
+        {
+            compClass = typeof(Comps.ExplosiveNest);
+        }
+    }
+}
