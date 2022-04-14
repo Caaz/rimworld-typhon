6b2fc6f811a0ba50993b932a670631f7cf6f2646
Daniel Caaz <dcaazy@gmail.com>
2022-04-13 21:12:19 -0700

    Add mimic check before throwing more mimics down

--- Source/StorytellerComp/EnsureTyphonPresence.cs
+++ Source/StorytellerComp/EnsureTyphonPresence.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using RimWorld;
+using Verse;
 
 namespace Typhon.StorytellerComp
 {
@@ -7,12 +8,22 @@ namespace Typhon.StorytellerComp
     {
         public override IEnumerable<FiringIncident> MakeIntervalIncidents(IIncidentTarget target)
         {
-            IncidentDef shipChunkDrop = TyphonDefOf.Incident.MimicCrash;
-            IncidentParms parms = GenerateParms(shipChunkDrop.category, target);
-            if (shipChunkDrop.Worker.CanFireNow(parms))
-            {
-                yield return new FiringIncident(shipChunkDrop, this, parms);
-            }
+            Map map = target as Map;
+            if (
+                !Rand.MTBEventOccurs(.5f, 60000, 1000f) 
+                || (map == null)
+                || HasMimic(map)
+            ) yield break;
+
+            IncidentDef mimicCrash = TyphonDefOf.Incident.MimicCrash;
+            IncidentParms parms = GenerateParms(mimicCrash.category, target);
+            if (mimicCrash.Worker.CanFireNow(parms))
+                yield return new FiringIncident(mimicCrash, this, parms);
+        }
+        private bool HasMimic(Map map)
+        {
+            return map.listerThings.ThingsOfDef(TyphonDefOf.Thing.Typhon_Mimic).Any()
+                || map.listerThings.ThingsOfDef(TyphonDefOf.Thing.Typhon_Mimic_Hidden).Any();
         }
     }
 }
