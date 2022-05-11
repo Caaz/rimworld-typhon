ecbde15e048d75cf7ef1d74ecde820e6da9fe285
Daniel Caaz <dcaazy@gmail.com>
2022-05-10 17:59:00 -0700

    WIP

---
+++ Source/StorytellerCompProperties/RandomWithEmpathy.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace Typhon.StorytellerCompProperties
+{
+    class RandomWithEmpathy : StorytellerCompProperties_RandomMain
+    {
+        public RandomWithEmpathy()
+        {
+            compClass = typeof(StorytellerComp.RandomWithEmpathy);
+        }
+    }
+}
