d9d10571a9e537128a1f89ddf78636e36a1755e8
Daniel Caaz <dcaazy@gmail.com>
2022-12-17 23:44:20 -0800

    Add jobgiver for casting hostile abilities

---
+++ Source/JobGiver/CastHostileAbility.cs
@@ -0,0 +1,28 @@
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace Typhon.JobGiver
+{
+    public class CastHostileAbility : JobGiver_AICastAbility
+    {
+        protected override LocalTargetInfo GetTarget(Pawn pawn, Ability ability)
+        {
+            LocalTargetInfo target = pawn.CurJob.targetA;
+            if (!target.HasThing) return null;
+
+            IntVec3 targetLocation = target.Cell;
+            float num = pawn.Position.DistanceTo(targetLocation);
+            VerbProperties verbProps = ability.verb.verbProps;
+            if (num < verbProps.minRange || num > verbProps.range || !GenSight.LineOfSight(pawn.Position, targetLocation, pawn.Map))
+            {
+                return null;
+            }
+            if (!ability.verb.ValidateTarget(target, showMessages: false))
+            {
+                return null;
+            }
+            return target;
+        }
+    }
+}
